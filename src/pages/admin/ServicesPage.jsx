3ca8956deed03d70875b90e84287f8f8367bb3b1
natehc <chetanchetu4940@gmail.com>
2025-12-17 22:23:56 +0530

    Fixed the Service page text display issue

--- src/pages/admin/ServicesPage.jsx
+++ src/pages/admin/ServicesPage.jsx
@@ -316,8 +316,8 @@ export default function ServicesPage() {
                   onChange={(e) =>
                     setFormData({ ...formData, price: e.target.value })
                   }
-                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all text-gray-900 bg-white"
-                  placeholder="Starting at $500"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none  text-gray-900 bg-white"
+                  placeholder="Starting at $500"transition-all
                 />
               </div>
 
